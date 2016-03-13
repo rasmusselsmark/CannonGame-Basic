5286587b6149e4ee221a1bc173374b890b92c012
Rasmus Selsmark <rasmus@unity3d.com>
2016-03-13 15:37:20 +0200

    Stop cannonball when hitting ground (using script)

---
+++ Assets/Scripts/CannonBall.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class CannonBall : MonoBehaviour
+{
+	void OnCollisionEnter2D(Collision2D coll)
+	{
+		if (coll.gameObject.name == "Grass")
+		{
+			GetComponent<Rigidbody2D> ().isKinematic = true;
+			GetComponent<Rigidbody2D> ().velocity = Vector3.zero;
+		}
+	}
+}
