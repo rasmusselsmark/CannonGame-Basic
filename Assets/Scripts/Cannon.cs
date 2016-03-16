96902728410f10b8f01f0b82c8af7b0fe1781b3d
Rasmus Selsmark <rasmus@unity3d.com>
2016-03-16 03:28:08 +0200

    Added explosion plus setting collider and tag for cannons

--- Assets/Scripts/Cannon.cs
+++ Assets/Scripts/Cannon.cs
@@ -5,18 +5,39 @@ public class Cannon : MonoBehaviour
 {
     public GameObject CannonBarrel;
     public GameObject CannonBallPrefab;
+    public GameObject ExplosionPrefab;
 
     public KeyCode KeyUp;
     public KeyCode KeyDown;
     public KeyCode KeyFire;
 
-    public UnityEngine.UI.Text ScoreText;
+    public UnityEngine.UI.Text ScoreUIText;
 
     private float _firePower = 10f;
+    private int _score;
+
+    public int Score
+    {
+        get { return _score; }
+        set
+        {
+            _score = value;
+
+            if (ScoreUIText)
+                ScoreUIText.text = _score.ToString();
+        }
+    }
 
     // Use this for initialization
     void Start ()
     {
+        Reset ();
+    }
+
+    public void Reset()
+    {
+        gameObject.SetActive (true);
+        CannonBarrel.transform.localRotation = Quaternion.identity;
     }
 
     // Update is called once per frame
@@ -51,9 +72,25 @@ public class Cannon : MonoBehaviour
                               position,
                               CannonBarrel.transform.rotation) as GameObject;
 
+        cannonBall.GetComponent<CannonBall> ().FiringPlayer = this;
         cannonBall.GetComponent<Rigidbody2D> ().AddForce (
             CannonBarrel.transform.right * power,
             ForceMode2D.Impulse);
         Destroy (cannonBall.gameObject, 30);
+    }
+
+    public void GotHit ()
+    {
+        if (ExplosionPrefab)
+        {
+            GameObject explosion = Instantiate (
+                ExplosionPrefab,
+                this.transform.position,
+                Quaternion.identity) as GameObject;
+            Destroy (explosion, 1f);
+        }
+
+        this.gameObject.SetActive (false);
+        Invoke ("Start", 2);
     }
 }
--- Assets/Scripts/CannonBall.cs
+++ Assets/Scripts/CannonBall.cs
@@ -2,12 +2,27 @@
 
 public class CannonBall : MonoBehaviour
 {
+    public Cannon FiringPlayer;
+
     void OnCollisionEnter2D (Collision2D collision)
     {
         if (collision.gameObject.name == "Grass")
         {
             GetComponent<Rigidbody2D> ().isKinematic = true;
             GetComponent<Rigidbody2D> ().velocity = Vector3.zero;
+        }
+        else if (collision.gameObject.tag == "Cannon")
+        {
+            Cannon cannon = collision.gameObject.GetComponent<Cannon> ();
+            cannon.GotHit ();
+
+            if (collision.gameObject.name != FiringPlayer.name)
+            {
+                FiringPlayer.Score += 1;
+                FiringPlayer.Reset ();
+            }
+
+            Destroy (this.gameObject);
         }
     }
 }
